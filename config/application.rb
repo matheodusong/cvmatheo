a1e3b546424db749c8640ec215568efef8f78bdc
Matheo Dusong <matheo@dusong.ch>
2018-03-06 20:48:29 +0100

    config assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Cvmatheo
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     config.generators do |generate|
       generate.assets false
       generate.helper false
